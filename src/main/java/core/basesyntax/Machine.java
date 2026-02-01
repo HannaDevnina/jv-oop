6aa188ae383c64dc85a9b2eb9cb9416b7c2031c9
Hanna Devnina <annadevnina@gmail.com>
2026-02-01 09:37:05 -0600

    added Machine class and its methods doWork(), and stopWork()

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import jdk.dynalink.linker.GuardedInvocationTransformer;
+
+public abstract class Machine {
+    public abstract void doWork();
+
+    public abstract void stopWork();
+}
+
+
